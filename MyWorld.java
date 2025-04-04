83cd8f8f8c9656e2c4402233bfc1dcabcfb64df8
vviiii17 <150176425+vviiii17@users.noreply.github.com>
2025-04-04 14:02:47 -0400

    background update

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    private GreenfootImage background;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,5 +18,8 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
+        background = new GreenfootImage("background01.png");
+        setBackground(background);
+
     }
 }
