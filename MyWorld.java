3ba7f271414d6f35b0b328a05445ad6c9b3bf68a
bixie0987 <chinen.hu@gmail.com>
2025-04-07 09:20:37 -0400

    added placeholder for researcher

--- MyWorld.java
+++ MyWorld.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     private GreenfootImage background;
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 
 =======
@@ -17,15 +18,25 @@ public class MyWorld extends World
     private int spiderXSpawn = 100;
     
 >>>>>>> subs
+=======
+    
+    //x spawn coordinate for spiders. this is set for the team on the left
+    private int spiderXSpawn = 100;
+
+>>>>>>> Stashed changes
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
+<<<<<<< Updated upstream
 <<<<<<< HEAD
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
 =======
+=======
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+>>>>>>> Stashed changes
         // Create a new world with 1024x800 cells with a cell size of 1x1 pixels.
 >>>>>>> subs
         super(1024, 800, 1); 
@@ -50,9 +61,16 @@ public class MyWorld extends World
             spiderXSpawn = 100; //sets spider x coordinate to left side
         }
         if(randNum < spawnChance){ //chance for a spider to spawn. change logic
+<<<<<<< Updated upstream
             addObject(new Spider("Right"), 300, 800); //added random nums for x and y for now
             addObject(new Spider(), spiderXSpawn, 600); //added random nums for x and y for now
             //x and y should change based on team
+=======
+            if(randNum < spawnChance) { //chance for a spider to spawn. change logic//added random nums for x and y for now
+                addObject(new Spider(), spiderXSpawn, 600); //added random nums for x and y for now
+                //x and y should change based on team
+            }
+>>>>>>> Stashed changes
         }
     }
 }
