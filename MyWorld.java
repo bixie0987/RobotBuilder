c36674246566c90388bb1ef298a3bcceeeaaf660
vviiii17 <150176425+vviiii17@users.noreply.github.com>
2025-04-08 08:45:25 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -9,36 +9,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     private GreenfootImage background;
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-
-=======
     
     //x spawn coordinate for spiders. this is set for the team on the left
     private int spiderXSpawn = 100;
-    
->>>>>>> subs
-=======
-    
-    //x spawn coordinate for spiders. this is set for the team on the left
-    private int spiderXSpawn = 100;
-
->>>>>>> Stashed changes
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-=======
-=======
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
->>>>>>> Stashed changes
-        // Create a new world with 1024x800 cells with a cell size of 1x1 pixels.
->>>>>>> subs
         super(1024, 800, 1); 
         background = new GreenfootImage("background01.png");
         setBackground(background);
@@ -46,7 +25,6 @@ public class MyWorld extends World
     }
     
     public void act(){
-        spawn();
         spawn("Right");
         spawn("Left");
     }
@@ -61,16 +39,12 @@ public class MyWorld extends World
             spiderXSpawn = 100; //sets spider x coordinate to left side
         }
         if(randNum < spawnChance){ //chance for a spider to spawn. change logic
-<<<<<<< Updated upstream
-            addObject(new Spider("Right"), 300, 800); //added random nums for x and y for now
             addObject(new Spider(), spiderXSpawn, 600); //added random nums for x and y for now
             //x and y should change based on team
-=======
             if(randNum < spawnChance) { //chance for a spider to spawn. change logic//added random nums for x and y for now
                 addObject(new Spider(), spiderXSpawn, 600); //added random nums for x and y for now
                 //x and y should change based on team
             }
->>>>>>> Stashed changes
         }
     }
 }
