274afc95db60ebf559ee61c7fa916f9ab3667892
bixie0987 <chinen.hu@gmail.com>
2025-04-08 09:15:59 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -62,7 +62,7 @@ public class MyWorld extends World
         }
 
 
-        if(randNum < spawnChance) { //chance for a spider to spawn. change logic//added random nums for x and y for now
+        if(randomNum == spawnChance){ //chance for a spider to spawn. change logic//added random nums for x and y for now
             addObject(new Spider(), spiderXSpawn, 600); //added random nums for x and y for now
             //x and y should change based on team
         }
