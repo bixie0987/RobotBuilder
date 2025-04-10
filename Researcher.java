f17f60db41324343c37b21470f529c31588525bd
bixie0987 <chinen.hu@gmail.com>
2025-04-10 09:17:27 -0400

    researcher attacks opponent

--- Researcher.java
+++ Researcher.java
@@ -8,50 +8,77 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Researcher extends Scientist
 {
+    private boolean firstTime = false;
+    private int startX, startY;
+    private String side; //Whether the researchers are on the left or right side.
     /**
      * Act - do whatever the Researcher wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-
     public Researcher () {
         setImage("R-Placeholder.png");
         GreenfootImage img = getImage();
-        img.scale(img.getWidth() / 2, img.getHeight() / 2);
+        img.scale(img.getWidth() / 4, img.getHeight() / 4);
         setImage(img);
     }
 
     public void act()
     {
+        if (firstTime == false) {
+            if (getX() < 301) {
+                side = "left";
+            } else {
+                side = "right";
+            }
+            startX = getX();
+            startY = getY();
+            firstTime = true;
+        }
         // Add your action code here.
         killSpider();
     }
-
     public void killSpider () {
         Spider target = (Spider) getOneIntersectingObject(Spider.class);
         if (target != null) {
             getWorld().removeObject(target);
-        } else {
+        } else if (hasSpiderOnSide()) {
             Spider closest = getClosestSpider();
             if (closest != null) {
                 turnTowards(closest.getX(), closest.getY());
                 move(2);
             }
+        } else if (startX != getX() && startY != getY()) {
+            turnTowards(startX, startY);
+            move(2);
         }
     }
-    
+
     public Spider getClosestSpider() {
         Spider closest = null;
         double closestDistance = Double.MAX_VALUE;
-
+        double distance;
         for (Object obj : getWorld().getObjects(Spider.class)) {
             Spider spider = (Spider) obj;
-            double distance = Math.hypot(getX() - spider.getX(), getY() - spider.getY());
-            if (distance < closestDistance) {
-                closestDistance = distance;
-                closest = spider;
+            
+            int x = spider.getX();
+            if (x >= 256 && x <= 768) {
+                distance = Math.hypot(getX() - spider.getX(), getY() - spider.getY());
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closest = spider;
+                }
             }
         }
         return closest;
     }
+    public boolean hasSpiderOnSide() {
+        for (Object obj : getWorld().getObjects(Spider.class)) {
+            Spider spider = (Spider) obj;
+            
+            if ((side.equals("left") && spider.getX() < 512 && spider.getX() > 256) || (side.equals("right") && spider.getX() > 512 && spider.getX() < 768)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
-
