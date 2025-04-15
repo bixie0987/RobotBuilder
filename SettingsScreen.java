8d0a773cfca60e83f9ce00a3f13d28e63f2cd765
bixie0987 <chinen.hu@gmail.com>
2025-04-14 22:53:20 -0400

    remove print

--- SettingsScreen.java
+++ SettingsScreen.java
@@ -49,7 +49,6 @@ public class SettingsScreen extends World
             
             // Exit SettingsScreen and switch to simulation screen (MyWorld)
             Greenfoot.setWorld(new MyWorld());
-            ParamStorage.printAll();
         }
     }
 }
