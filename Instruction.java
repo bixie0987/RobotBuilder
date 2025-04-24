38deb79904424f99eec4dc5b210fab2569b3a336
bixie0987 <chinen.hu@gmail.com>
2025-04-23 21:52:33 -0400

    finished start screen

---
+++ Instruction.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Instruction here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Instruction extends Actor
+{
+    private boolean pressed = false;
+    
+    private GreenfootImage image;
+
+    /**
+     * Act - do whatever the Instruction wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Instruction() {
+        image = new GreenfootImage("instructions.png");
+        setImage(image);
+    }
+    public void act()
+    {
+        if(Greenfoot.mousePressed(this)) {
+            Sounds.getInstance().playSounds(Sounds.MOUSE_CLICK);
+            pressed = true;
+        } else {
+            pressed = false;
+        }
+        // Add your action code here.
+    }
+    public boolean getPressed() {
+        return pressed;
+    }
+}
--- StartScreen.java
+++ StartScreen.java
@@ -10,14 +10,16 @@ public class StartScreen extends World
 {
     private GreenfootImage background;
     
-    private int delay = 120;
+    private int delay = 198;
+    private boolean sound = false;
     private boolean researcher = false;
     private boolean supplier = false;
+    private boolean removed = false;
+    private boolean added = false;
     private int countdown1 = 300;
     private int countdown2 = 300;
     
-    private ResearcherText researcherText;
-    private SupplierText supplierText;
+    private Instruction instructions;
     /**
      * Constructor for objects of class StartScreen.
      * 
@@ -31,6 +33,10 @@ public class StartScreen extends World
     }
     
     public void act() {
+        if (!sound) {
+            Greenfoot.playSound("police_siren.wav");
+            sound = true;
+        }
         if (delay > 0) {
             delay--;
             return;
@@ -40,28 +46,36 @@ public class StartScreen extends World
     
     public void spawn() {
         if (!researcher) {
-            addObject(new ResearcherText(), 512, 400);
+            addObject(new ResearcherText(), 256, 300);
             researcher = true;
         }
         if (countdown1 > 0) {
             countdown1--;
             return;
         }
-        if (researcherText != null) {
-            removeObject(researcherText); // remove researcherText after countdown
-            researcherText = null;
-        }
         if (!supplier) {
-            addObject(new SupplierText(), 512, 400);
+            addObject(new SupplierText(), 768, 500);
             supplier = true;
         }
         if (countdown2 > 0) {
             countdown2--;
             return;
         }
-        if (supplierText != null) {
-            removeObject(supplierText); // remove supplierText after countdown
-            supplierText = null;
+        if (!removed) {
+            for (Object obj : getObjects(Actor.class)) {
+                Actor scientist = (Actor) obj;
+                removeObject(scientist);
+            }
+            removed = true;
+        }
+        if (!added) {
+            instructions = new Instruction();
+            addObject(instructions, 512, 400);
+            added = true;
+        }
+        if(instructions.getPressed()) {
+            // Exit StartScreen and switch to setting screen (SettingScreen)
+            Greenfoot.setWorld(new SettingsScreen());
         }
     }
 }
